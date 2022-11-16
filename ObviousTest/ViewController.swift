b9ca694ca5fc5dfdd152e4f3cdc6b2a0998d2cfc
Mohsin Khan <>
2022-11-16 16:08:35 +0530

    first commit

---
+++ ObviousTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ObviousTest
+//
+//  Created by MK on 16/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
